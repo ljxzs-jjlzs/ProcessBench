3d3a586abad2e392b37b9aca2bb69838a4262e39
ljxzs-jjlzs <021218ljx@gmail.com>
2025-03-30 10:50:41 +0800

    update

--- code/example.sh
+++ code/example.sh
@@ -1,15 +1,15 @@
 
-work_dir=/home/nfs05/liujx/GithubRepos/ProcessBench/code
+work_dir=""
 cd ${work_dir}
-
+model_path=""
 # CUDA_VISIBLE_DEVICES=0,1,6,7 python /home/nfs05/liujx/GithubRepos/ProcessBench/code/generate.py \
 #     --model_path /home/nfs05/model/Qwen2.5-7B-Instruct \
 #     --configs gsm8k \
 #     --use_voting \
 #     --voting_n 2
 
-CUDA_VISIBLE_DEVICES=0,1,6,7 python /home/nfs05/liujx/GithubRepos/ProcessBench/code/run_eval.py \
-    --model_path /home/nfs05/model/Qwen2.5-7B-Instruct \
-    --configs gsm8k \
+python run_eval.py \
+    --model_path $model_path \
+    --configs gsm8k math olympiadbench omnimath \
     --use_voting \
-    --voting_n 2
+    --voting_n 16
